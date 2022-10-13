db1ae45534b78e0bc60a6441c25be494298527ec
Gérard Dethier <gerard@logion.network>
2022-10-13 14:54:21 +0200

    chore: upgrade to polkadot-v0.9.29.

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,6 @@
 use crate::{mock::*, Error};
 use frame_support::traits::WrapperKeepOpaque;
+use frame_support::weights::Weight;
 use frame_support::{assert_err, assert_ok};
 use sp_runtime::traits::BlakeTwo256;
 use sp_runtime::traits::Hash;
@@ -8,7 +9,7 @@ use sp_runtime::traits::Hash;
 fn it_requests_call_if_not_legal_officer() {
 	new_test_ext().execute_with(|| {
 		let call_hash = BlakeTwo256::hash_of(&"call-bytes".as_bytes().to_vec());
-		assert_ok!(Vault::request_call(Origin::signed(USER_ID), vec![LEGAL_OFFICER1, LEGAL_OFFICER2], call_hash.to_fixed_bytes(), 10000));
+		assert_ok!(Vault::request_call(Origin::signed(USER_ID), vec![LEGAL_OFFICER1, LEGAL_OFFICER2], call_hash.to_fixed_bytes(), Weight::from_ref_time(10000)));
 	});
 }
 
@@ -16,7 +17,7 @@ fn it_requests_call_if_not_legal_officer() {
 fn it_fails_requesting_call_if_legal_officer() {
 	new_test_ext().execute_with(|| {
 		let call_hash = BlakeTwo256::hash_of(&"call-bytes".as_bytes().to_vec());
-		assert_err!(Vault::request_call(Origin::signed(LEGAL_OFFICER1), vec![LEGAL_OFFICER1, LEGAL_OFFICER2], call_hash.to_fixed_bytes(), 10000), Error::<Test>::WrongInitiator);
+		assert_err!(Vault::request_call(Origin::signed(LEGAL_OFFICER1), vec![LEGAL_OFFICER1, LEGAL_OFFICER2], call_hash.to_fixed_bytes(), Weight::from_ref_time(10000)), Error::<Test>::WrongInitiator);
 	});
 }
 
@@ -24,7 +25,7 @@ fn it_fails_requesting_call_if_legal_officer() {
 fn it_fails_requesting_call_if_not_two_legal_officers() {
 	new_test_ext().execute_with(|| {
 		let call_hash = BlakeTwo256::hash_of(&"call-bytes".as_bytes().to_vec());
-		assert_err!(Vault::request_call(Origin::signed(USER_ID), vec![LEGAL_OFFICER1], call_hash.to_fixed_bytes(), 10000), Error::<Test>::InvalidSignatories);
+		assert_err!(Vault::request_call(Origin::signed(USER_ID), vec![LEGAL_OFFICER1], call_hash.to_fixed_bytes(), Weight::from_ref_time(10000)), Error::<Test>::InvalidSignatories);
 	});
 }
 
@@ -32,7 +33,7 @@ fn it_fails_requesting_call_if_not_two_legal_officers() {
 fn it_fails_requesting_call_if_not_all_legal_officers() {
 	new_test_ext().execute_with(|| {
 		let call_hash = BlakeTwo256::hash_of(&"call-bytes".as_bytes().to_vec());
-		assert_err!(Vault::request_call(Origin::signed(USER_ID), vec![LEGAL_OFFICER1, ANOTHER_USER_ID], call_hash.to_fixed_bytes(), 10000), Error::<Test>::InvalidSignatories);
+		assert_err!(Vault::request_call(Origin::signed(USER_ID), vec![LEGAL_OFFICER1, ANOTHER_USER_ID], call_hash.to_fixed_bytes(), Weight::from_ref_time(10000)), Error::<Test>::InvalidSignatories);
 	});
 }
 
@@ -41,6 +42,6 @@ fn it_approves_call_if_two_other_signatories() {
 	new_test_ext().execute_with(|| {
 		let call = WrapperKeepOpaque::from_encoded("call-bytes".as_bytes().to_vec());
 		let timepoint = Default::default();
-		assert_ok!(Vault::approve_call(Origin::signed(LEGAL_OFFICER1), vec![USER_ID, LEGAL_OFFICER2], call, timepoint, 10000));
+		assert_ok!(Vault::approve_call(Origin::signed(LEGAL_OFFICER1), vec![USER_ID, LEGAL_OFFICER2], call, timepoint, Weight::from_ref_time(10000)));
 	});
 }
